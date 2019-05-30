dfff9988535a8aa18f064c68536c66295881d68f
steephen varghese <steephen.varghese@cubettech.com>
2019-05-30 17:40:20 +0530

    lint issue fix

--- src/db.init.ts
+++ src/db.init.ts
@@ -10,8 +10,7 @@ lme.i('> connecting to ' + MONGO_URI);
 // Promisifying all mongoose methods
 mongoose.Promise = BluePromise;
 
-export const mongooseConnectionPromise = mongoose.connect(
-  MONGO_URI, {
+export const mongooseConnectionPromise = mongoose.connect(MONGO_URI, {
   useNewUrlParser: true,
   useCreateIndex: true,
 });
